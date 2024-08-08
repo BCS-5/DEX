82904155cbc920a077c2186f1e0d18ba6ec0c289
asm9677 <asm9677@naver.com>
2024-08-08 09:01:38 +0900

    feature: Order History

--- vite/src/components/Trade/OrderHistory.tsx
+++ vite/src/components/Trade/OrderHistory.tsx
@@ -1,17 +1,33 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 const OrderHistory: FC = () => {
+  const [selectedMenu, setSelectedMenu] = useState<number>(0);
   return (
-    <div className="flex flex-col w-full h-full bg-red-100">
-      <div className="flex justify-between w-full h-12 items-center px-4">
+    <div className="flex flex-col w-full h-full bg-[#131722] text-[#72768f] font-semibold border-t-[0.6px] border-r-[0.6px] border-[#363A45]">
+      <div className="flex justify-between w-full h-12 items-center px-6">
         <div className="flex gap-4 ">
-          <div>Position</div>
-          <div>Open Order</div>
-          <div>Trade History</div>
-          <div>Order History</div>
-          <div>Funding History</div>
+          <button
+            className={`${selectedMenu == 0 && "text-[#729aff]"}`}
+            onClick={() => setSelectedMenu(0)}
+          >
+            Position (0)
+          </button>
+          <button
+            className={`${selectedMenu == 1 && "text-[#729aff]"}`}
+            onClick={() => setSelectedMenu(1)}
+          >
+            Order (0)
+          </button>
+          <button
+            className={`${selectedMenu == 2 && "text-[#729aff]"}`}
+            onClick={() => setSelectedMenu(2)}
+          >
+            History
+          </button>
         </div>
         <div className="flex">
-          <div>All My Position &gt;</div>
+          <button className="text-[#729aff]">Close All</button>
+          <div className="border-r-[0.6px] border-r-[#363A45] h-6 mx-4"></div>
+          <button>All My Position &gt;</button>
         </div>
       </div>
       <div></div>
