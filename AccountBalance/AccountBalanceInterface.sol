3f94b17607d6bf848e32c30881c77e3abcefd79b
Yeongho Jeon <58592207+wndrbrwn@users.noreply.github.com>
2024-08-09 17:27:56 +0900

    Update AccountBalanceInterface.sol

--- AccountBalance/AccountBalanceInterface.sol
+++ AccountBalance/AccountBalanceInterface.sol
@@ -1,16 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.2 < 0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
-interface AccountBalanceInterface {
-    // 이벤트
-    event FundingPaymentSettled(address indexed trader, address indexed baseToken, int256 longFundingPayment, int256 shortFundingPayment);
-    event LiquidationWarning(address indexed trader, address indexed baseToken, int256 positionSize, uint256 marginRatio);
+import "./IClearinghouse.sol";
 
-    // 함수
-    function updateFundingRate(address baseToken) external;
-    function settleFundingPayment(address trader, address baseToken) external;
-    function getPositionSize(address trader, address baseToken) external view returns (int256);
-    function checkLiquidation(address trader, address baseToken, int256 positionSize, int256 openNotional, int256 unrealizedPnl) external view;
-    function getPrice(address baseToken) external view returns (uint256);
+interface IAccountBalance {
+    //함수
+    function setKeeper(address newKeeper) external;
     function getMarkPrice(address baseToken) external view returns (uint256);
+    function setIndexPrice(address baseToken, uint256 price) external;
+    function getIndexPrice(address baseToken) external view returns (uint256);
+    function setOpenInterest(address baseToken, int256 positionSize, bool isLong) external;
+    function getLongOpenInterest(address baseToken) external view returns (uint256);
+    function getShortOpenInterest(address baseToken) external view returns (uint256);
+    function checkLiquidation(
+        IClearinghouse.Position memory position,
+        address baseToken
+    ) external view returns (bool);
+
+    // Public 상태 변수들에 대한 getter 함수들
+    function lastFundingTimes(address baseToken) external view returns (uint256);
+    function cumulativeLongFundingRates(address baseToken) external view returns (int256);
+    function cumulativeShortFundingRates(address baseToken) external view returns (int256);
+    function userLongFundingPayments(address baseToken, address user) external view returns (int256);
+    function userShortFundingPayments(address baseToken, address user) external view returns (int256);
 }
