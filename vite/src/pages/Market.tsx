c756c75a0b3d1d0669d8e23dd4b87aaa3e777b16
wndrbrwn <jyh7678@hotmail.com>
2024-08-14 15:54:23 +0900

    MarketTable

--- vite/src/pages/Market.tsx
+++ vite/src/pages/Market.tsx
@@ -1,7 +1,17 @@
-import { FC } from "react";
+import React from 'react';
+import MarketFilter from '../components/Market/MarketFilter';
+import MarketTable from '../components/Market/MarketTable';
 
-const Market: FC = () => {
-  return <div>Market</div>;
-};
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-6">Markets</h1>
+        <MarketFilter />
+        <MarketTable />
+      </main>
+    </div>
+  );
+}
 
-export default Market;
+export default App;
