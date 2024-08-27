3c533d5b9c7dbfefe549e736e1af3fd7e43a0a5b
asm9677 <asm9677@naver.com>
2024-08-27 19:46:45 +0900

    vercel

--- vite/src/components/Home/Footer.tsx
+++ vite/src/components/Home/Footer.tsx
@@ -1,12 +1,12 @@
 import { VscGithub } from "react-icons/vsc";
-import FooterImg from "./img/logo/footerImg.png";
+// import FooterImg from "./img/logo/footerImg.png";
 
 const Footer = () => {
   return (
     <section className="max-w-[1080px] mx-auto flex justify-between pt-[50px] pb-[75px] px-40 ">
       <div className="pt-5 text-[14px] text-white">Copyright © 2024 fiX</div>
       <div className=" text-[14px] pr-[100px]">
-        <img src={FooterImg} alt="Logo" className="w-11" />
+        {/* <img src={FooterImg} alt="Logo" className="w-11" /> */}
       </div>
       <button className=" text-[25px] pt-3 text-white">
         <a href="https://github.com/BCS-5/DEX">
